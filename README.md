a2f5ea063703181d696389ff954be95df006b6a4
Vera Juan <veracodeJuan@gmail.com>
2024-08-14 19:57:43 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# [:] Example PHP project using Composer.
+
+An example PHP project using Composer to demonstrate [SourceClear](https://www.sourceclear.com) scans.
+
+## Install and activate SourceClear
+Follow the instructions under the section "Setup and Configuration" in https://www.sourceclear.com/docs/command-line-interface/ to install and activate our SourceClear agent.
+
+## Scan this project
+`srcclr scan --url https://github.com/srcclr/example-php-composer`
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'srcclr/example-php-composer',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '6ecccfcefbba05b978e187840f3e8007ff495d9d',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v5.2.9',
+            'version' => '5.2.9.0',
+            'reference' => '73b61679809615850706f1dbf88e6df2ddc05745',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'srcclr/example-php-composer' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '6ecccfcefbba05b978e187840f3e8007ff495d9d',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
